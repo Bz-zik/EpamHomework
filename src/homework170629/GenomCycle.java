974e4de8d977716b2775fd8e3eb2d02d350613ae
Bz-zik <narciss95@yandex.ru>
2017-06-30 14:14:45 +0300

    released GenomSort

--- src/homework170629/GenomCycle.java
+++ src/homework170629/GenomCycle.java
@@ -3,7 +3,7 @@ package homework170629;
 public class GenomCycle {
 
     public static void main(String[] args) {
-        byte[] data = DataGenerator.generate(30);
+        byte[] data = DataGenerator.generate(5000);
 
         for (byte b :
                 data) {
@@ -12,12 +12,17 @@ public class GenomCycle {
 
         System.out.println();
 
+        long start = System.currentTimeMillis();
+
         findDuplicates(data, 5);
+
+        long finish = System.currentTimeMillis();
+        System.out.println((finish - start) + " ms in work");
     }
 
     private static void findDuplicates(byte[] data, int w) {
         for (int i = 0; i < data.length - w; i++) {
-            for (int j = i+1; j < data.length - w; j++) {
+            for (int j = i+1; j < data.length - w + 1; j++) {
                 for (int k = 0; k < w; k++) {
                     if (data[i+k] != data[j+k])
                         break;
--- src/homework170629/GenomSort.java
+++ src/homework170629/GenomSort.java
@@ -1,4 +1,59 @@
 package homework170629;
 
+import java.util.Arrays;
+
 public class GenomSort {
+
+    public static void main(String[] args) {
+        byte[] data = DataGenerator.generate(5000);
+
+        for (byte b :
+                data) {
+            System.out.print((char)b);
+        }
+
+        System.out.println();
+
+        long start = System.currentTimeMillis();
+
+        findDuplicates(data, 5);
+
+        long finish = System.currentTimeMillis();
+
+        System.out.println((finish - start) + " ms in work");
+
+
+    }
+
+    private static void findDuplicates(byte[] data, int w) {
+        String[] subDate = new String[data.length - w + 1];
+
+        for (int i = 0; i < subDate.length; i++) {
+            for (int j = i; j < i + w; j++) {
+                if (subDate[i] == null) subDate[i] = "" + (char)data[j];
+                else subDate[i] += (char)data[j];
+            }
+        }
+
+        Arrays.sort(subDate);
+
+//        for (String s :
+//                subDate) {
+//            System.out.println(s);
+//        }
+
+        int count = 0;
+
+        for (int i = 1; i < subDate.length; i++) {
+            if (subDate[i].equals(subDate[i-1])) count++;
+            else {
+                if (count != 0) {
+                    System.out.println(subDate[i-1] + ": " + (count + 1) + " dublicates");
+                    count = 0;
+                }
+            }
+        }
+    }
+
+
 }
