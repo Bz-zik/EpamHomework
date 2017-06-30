33f832ced8a1c0442b97c946cff8b51be5890d1b
Bz-zik <narciss95@yandex.ru>
2017-06-30 14:42:59 +0300

    released GenomHashSet

--- src/homework170629/GenomHashSet.java
+++ src/homework170629/GenomHashSet.java
@@ -1,4 +1,43 @@
 package homework170629;
 
+import java.util.HashSet;
+
 public class GenomHashSet {
+
+    public static void main(String[] args) {
+        byte[] data = DataGenerator.generate(5000);
+
+        for (byte b :
+                data) {
+            System.out.print((char)b);
+        }
+
+        System.out.println();
+
+        long start = System.currentTimeMillis();
+
+        findDuplicates(data, 5);
+
+        long finish = System.currentTimeMillis();
+
+        System.out.println((finish - start) + " ms in work");
+
+
+    }
+
+    private static void findDuplicates(byte[] data, int w) {
+
+        HashSet<String> subDate = new HashSet<>();
+        String temp = "";
+        for (int i = 0; i < data.length - w + 1; i++) {
+            for (int j = i; j < i + w; j++) {
+                temp += (char)data[j];
+            }
+            if (subDate.contains(temp)) System.out.println("Duplicate was found: i = " + i + " " + temp);
+            else subDate.add(temp);
+            temp = "";
+        }
+
+    }
+
 }
